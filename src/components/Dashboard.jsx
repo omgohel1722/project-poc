e0e19295359050f1c3bf3493fff7f0fac2267d9d
omgohel1722 <om.gohel@atqor.com>
2025-10-08 17:49:08 +0530

    Icon updated.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -309,8 +309,9 @@ const Dashboard = () => {
                         </>
                       ) : (
                         <>
+                          Submit Feedback
                           <svg
-                            className="w-5 h-5"
+                            className="w-5 h-5 rotate-90"
                             fill="none"
                             stroke="currentColor"
                             viewBox="0 0 24 24"
@@ -322,7 +323,6 @@ const Dashboard = () => {
                               d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
                             />
                           </svg>
-                          Submit Feedback
                         </>
                       )}
                     </button>
